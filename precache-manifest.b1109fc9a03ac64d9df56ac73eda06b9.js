61f6a91a7772818eb685ca599ac00d3bcda7b525
jp956956 <38661378+jp956956@users.noreply.github.com>
2020-11-16 08:16:10 +0000

    Deploying to gh-pages from master e175f64f48fa55fa2c30eb8ec87fadc815b9a9b8

---
+++ precache-manifest.b1109fc9a03ac64d9df56ac73eda06b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c129749775dc7841d4e",
+    "url": "css/chunk-vendors.713ba136.css"
+  },
+  {
+    "revision": "699b58ad1fb8bed455526db138d86463",
+    "url": "index.html"
+  },
+  {
+    "revision": "6ef6b2a74bc77119a902",
+    "url": "js/app.3eccc863.js"
+  },
+  {
+    "revision": "9c129749775dc7841d4e",
+    "url": "js/chunk-vendors.f900ca0b.js"
+  },
+  {
+    "revision": "f3399ba7a5fc8294f746",
+    "url": "js/list.838d080a.js"
+  },
+  {
+    "revision": "b143e8d031ea1dc9fd13",
+    "url": "js/settings.7704203f.js"
+  },
+  {
+    "revision": "982672f70a8ae0b8927c7e9ed7d470cb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "sounds/alarm1.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "sounds/yay.mp3"
+  }
+]);
